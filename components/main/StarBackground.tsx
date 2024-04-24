c549a9a2beb6ce96ef85ccc49970343f7b1683e2
Shohel Kashyapa <71594964+Shohel-kashyapa@users.noreply.github.com>
2024-04-24 18:56:08 +0530

    starting StarBG

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Suspense, useRef, useState } from "react"
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
