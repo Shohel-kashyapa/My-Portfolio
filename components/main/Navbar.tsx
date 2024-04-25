afb3109df308aee70c8a0f4406a87c48397399f5
Shohel Kashyapa <71594964+Shohel-kashyapa@users.noreply.github.com>
2024-04-26 00:34:26 +0530

    Updateing

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           />
 
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Shohel Kashyapa
+            SK
           </span>
         </a>
 
