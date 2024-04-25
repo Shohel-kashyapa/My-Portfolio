c8f91b725a1bf2bcf70eb7f7b2d1986edb142e72
Shohel Kashyapa <71594964+Shohel-kashyapa@users.noreply.github.com>
2024-04-25 21:41:58 +0530

    Skills V2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
 
 export default function Page() {
     return (
         <main className="h-full w-full">
             <div className="flex flex-col gap-20">
                 <Hero />
+                <Skills />
             </div>
         </main>
     );
